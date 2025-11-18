db17b301f0804c98f6315ecd8f824dc6178e62d9
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-11-18 21:14:54 +0900

    [Gold V] Title: LCS, Time: 116 ms, Memory: 118572 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.py
@@ -0,0 +1,17 @@
+a = input()
+b = input()
+
+n = len(a)
+m = len(b)
+
+arr = [[0] * (m+1) for _ in range(n+1)]
+
+for i in range(1, n+1):
+    for j in range(1, m+1):
+
+        if a[i-1] == b[j-1]:
+            arr[i][j] = arr[i-1][j-1] + 1
+        else:
+            arr[i][j] = max(arr[i-1][j], arr[i][j-1])
+
+print(arr[n][m])
