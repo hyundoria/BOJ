5f72ed6f39dbb24d6aaca926fdbf374de5064e47
hyundoria <110231201+hyundoria@users.noreply.github.com>
2025-12-03 01:05:11 +0900

    [Gold IV] Title: 플로이드, Time: 188 ms, Memory: 111644 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,34 @@
+import sys
+
+INF = float('inf')
+
+def floyd_warshall():
+
+    for k in range(1,n+1):
+        for i in range(1,n+1):
+            for j in range(1, n+1):
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j])
+
+
+
+n = int(input())
+m = int(input())
+
+d = [[INF] * (n + 1) for _ in range(n + 1)]
+
+for i in range(1, n + 1):
+    d[i][i] = 0
+
+for _ in range(m):
+    a,b,c = map(int, input().split())
+    d[a][b] = min(d[a][b], c)
+
+floyd_warshall()
+
+for i in range(1,n+1):
+    for j in range(1, n+1):
+        if d[i][j] == INF:
+            print(0, end=' ')
+        else:
+            print(d[i][j], end=' ')
+    print()
