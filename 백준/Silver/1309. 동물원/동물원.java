d14fd7f58dd1584e5686eb824ec75b9b68fef7a0
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 11:59:41 +0900

    [Silver I] Title: 동물원, Time: 172 ms, Memory: 18484 KB -BaekjoonHub

---
+++ 백준/Silver/1309. 동물원/동물원.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+
+        int n = sc.nextInt();
+
+        long[] dp = new long[n+1];
+
+        dp[0] = 1;
+        dp[1] = 3;
+
+        int mod = 9901;
+        
+        for(int i=2; i<=n; i++){
+
+            dp[i] = ((dp[i-1] * 2) + dp[i-2]) % mod;
+            
+        }
+
+        System.out.print(dp[n]);
+        
+    }
+}
