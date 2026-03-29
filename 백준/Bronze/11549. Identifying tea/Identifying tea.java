2b29a150ecc6f201916b4753f14b9652ea0a4c83
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:04:16 +0900

    [Bronze IV] Title: Identifying tea, Time: 204 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/11549. Identifying tea/Identifying tea.java
@@ -0,0 +1,24 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+
+        int cnt = 0;
+
+        for (int i=0; i<5; i++) {
+            int k = sc.nextInt();
+            if (k == t) {
+                cnt++;
+            }
+        }
+
+
+        System.out.println(cnt);
+
+    }
+}
