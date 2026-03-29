aab0192cf9ae0c6170d8164700054516b426ddfd
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:16:21 +0900

    [Bronze IV] Title: 주사위 세개, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -0,0 +1,24 @@
+a, b, c =  input().split()
+
+a = int(a)
+b = int(b)
+c = int(c)
+
+
+if a == b == c :
+  print(10000+a*1000)
+  
+elif a == b != c:
+  print(1000+a*100)
+elif a != b == c:
+  print(1000+b*100)
+elif a == c != b:
+  print(1000+a*100)
+
+else:
+  if (a > b and a > c):
+    print(a*100)
+  elif (b > a and b > c):
+    print(b*100)
+  elif (c > a and c > b):
+    print(c*100)
