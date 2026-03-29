23ac1a0159052775628b0017cc79bc96814cf35c
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 15:00:17 +0900

    [Bronze IV] Title: 햄버거 만들기, Time: 208 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/25628. 햄버거 만들기/햄버거 만들기.java
@@ -0,0 +1,18 @@
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        try (Scanner sc = new Scanner(System.in)) {
+
+        int a = sc.nextInt()/2;
+        int b = sc.nextInt();
+
+        System.out.println(Math.min(a,b));
+
+
+
+
+        }
+    }
+}
