c709159b3a2baeaaf02377cd6b6c45321fdafe07
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 11:56:28 +0900

    [Silver I] Title: RGB거리, Time: 228 ms, Memory: 20192 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);        
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[n+1][3];
+
+        for(int i=1; i<=n; i++){
+            for(int j=0; j<3; j++){
+                
+                arr[i][j] = sc.nextInt();                
+            }
+        }
+
+        int[][] dp = new int[n+1][3];
+
+        dp[0][0] = arr[0][0];
+        dp[0][1] = arr[0][1];
+        dp[0][2] = arr[0][2];
+        
+        for(int i=1; i<=n; i++){
+            
+           dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + arr[i][0];
+           dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + arr[i][1];
+           dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + arr[i][2];
+            
+        }
+
+        System.out.print(Math.min(Math.min(dp[n][0], dp[n][1]), dp[n][2]));
+        
+    }
+}
