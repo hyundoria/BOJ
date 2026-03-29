0adda7728f0f22b90fb6be542bb43bec0770e370
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:57:31 +0900

    [Silver IV] Title: 덱, Time: 464 ms, Memory: 22772 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,77 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        Deque<Integer> deque = new LinkedList<>();
+
+        for (int i = 0; i < n; i++) {
+
+            String[] str = br.readLine().split(" ");
+
+            switch (str[0]) {
+                case "push_front":
+                    deque.addFirst(Integer.parseInt(str[1]));
+                    break;
+                case "push_back":
+                    deque.addLast(Integer.parseInt(str[1]));
+                    break;
+                case "pop_front":
+                    if (deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.pollFirst());
+                    }
+                    break;
+                case "pop_back":
+                    if (deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.pollLast());
+                    }
+                    break;
+                case "size":
+                    System.out.println(deque.size());
+                    break;
+                case "empty":
+                    if (deque.isEmpty()) {
+                        System.out.println(1);
+                    } else {
+                        System.out.println(0);
+                    }
+                    break;
+                case "front":
+                    if (deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.getFirst());
+                    }
+
+                    break;
+                case "back":
+                    if (deque.isEmpty()) {
+                        System.out.println(-1);
+                    } else {
+                        System.out.println(deque.getLast());
+                    }
+                    break;
+            }
+
+
+        }
+
+
+    }
+}
