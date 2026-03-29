86d368e77299eecdf86bc5e5c6106ef6683f1679
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:43:51 +0900

    [Bronze IV] Title: 타일 채우기 4, Time: 208 ms, Memory: 17764 KB -BaekjoonHub

---
+++ 백준/Bronze/15700. 타일 채우기 4/타일 채우기 4.java
@@ -0,0 +1,17 @@
+
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        long n = sc.nextLong();
+        long m = sc.nextLong();
+
+        System.out.println((n*m)/2);
+
+    }
+}
+
+
+
