40c08dd4afbffd19bf52ae5fdb082dc46ebcbd8b
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 12:00:22 +0900

    [Silver V] Title: 소트인사이드, Time: 244 ms, Memory: 18612 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,35 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args)  {
+        Scanner sc = new Scanner(System.in);
+
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[String.valueOf(n).length()];
+
+        int cnt = 0;
+
+        while (n > 0) {
+            arr[cnt++] = n%10;
+            n/=10;
+        }
+
+        Arrays.sort(arr);
+
+        String str = "";
+
+        for (int i=cnt-1; i>=0; i--) {
+            str += arr[i];
+        }
+
+        System.out.println(str);
+
+
+    }
+
+}
