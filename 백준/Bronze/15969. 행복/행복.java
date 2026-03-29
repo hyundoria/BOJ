908f375006041a5f0b6209df456d3483fe31a4c9
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:45:37 +0900

    [Bronze III] Title: 행복, Time: 276 ms, Memory: 19880 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15969. 행복/행복.java
@@ -0,0 +1,38 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        for (int i=0; i<n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int min = arr[0];
+        int max = arr[0];
+
+        for (int i=1; i<n; i++) {
+            if (min > arr[i]) {
+                min = arr[i];
+            }
+
+            if (max < arr[i]) {
+                max = arr[i];
+            }
+        }
+
+
+        System.out.println(max-min);
+
+
+    }
+}
+
+
+
