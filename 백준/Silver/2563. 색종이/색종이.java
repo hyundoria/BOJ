506fb67dab1cc9f4fe48cde16671ab4ffdb0e82a
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:19:06 +0900

    [Silver V] Title: 색종이, Time: 220 ms, Memory: 17860 KB -BaekjoonHub

---
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -0,0 +1,43 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[][] arr = new int[100][100];
+
+        for (int i = 0; i < n; i++) {
+
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+
+            for (int j = a; j < a+10; j++) {
+                for (int k = b; k < b+10; k++) {
+
+                    arr[j][k] = 1;
+
+                }
+            }
+
+        }
+
+        int cnt = 0;
+
+        for (int i = 0; i < 100; i++) {
+            for (int j = 0; j < 100; j++) {
+
+                if(arr[i][j] == 1) {
+                    cnt++;
+                }
+
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
+}
