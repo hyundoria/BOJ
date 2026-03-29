a358296817a1bab2f8bb4ad9fc68d2aa4abaeeb3
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:25:25 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 2852 ms, Memory: 334844 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -0,0 +1,30 @@
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        StringBuilder sb = new StringBuilder();
+
+        int n = sc.nextInt();
+
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        for (int i=0; i<n; i++) {
+            arr.add(sc.nextInt());
+        }
+
+        Collections.sort(arr);
+
+        for(int value : arr) {
+            sb.append(value).append('\n');
+        }
+        System.out.println(sb);
+
+
+    }
+}
