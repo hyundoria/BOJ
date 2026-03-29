814c0d5863122a8851f6c17416adbc2a6e6798f9
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:46:21 +0900

    [Bronze IV] Title: 더하기, Time: 196 ms, Memory: 17524 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+					
+			int n = sc.nextInt();
+			
+			for (int i = 0; i < n; i++) {
+				int T = sc.nextInt();
+				int sum = 0;
+				
+				for (int j = 0; j < T; j++) {
+					sum += sc.nextInt();
+				}
+				System.out.println(sum);
+			}
+		
+			
+		}		
+	}
+}
+
