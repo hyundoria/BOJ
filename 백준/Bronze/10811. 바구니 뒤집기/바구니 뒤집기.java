8e040b5993a49b1c6506124fcb47b2ad470a5cf8
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:54:57 +0900

    [Bronze II] Title: 바구니 뒤집기, Time: 252 ms, Memory: 18640 KB -BaekjoonHub

---
+++ 백준/Bronze/10811. 바구니 뒤집기/바구니 뒤집기.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        
+        int arr[] = new int[n];
+        
+         for(int i = 0; i < n; i++) {
+         
+             arr[i] = i+1;
+         }
+        
+        
+        for(int i = 0; i < m; i++) {
+            
+            int a = sc.nextInt()-1;
+            int b = sc.nextInt()-1;
+          
+            
+            int copy[] = arr.clone();
+            int k = 0;
+            for(int j = a; j<b+1; j++) {
+                
+                copy[b-k++] = arr[j];
+                
+            }
+            arr = copy.clone();
+        }
+    
+        for(int i = 0; i < n; i++) {
+            System.out.print(arr[i]+" ");
+        }
+    
+    
+    
+    }
+
+}
