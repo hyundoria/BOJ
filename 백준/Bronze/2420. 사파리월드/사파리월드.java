06d534852c0bdca41b22b22a664aa82b7b1f980a
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:13:08 +0900

    [Bronze V] Title: 사파리월드, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/2420. 사파리월드/사파리월드.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			
+		
+			long a = sc.nextLong();
+			long b = sc.nextLong();
+			
+			if (a>b) {
+				System.out.println(-b+a);
+			}
+			else if (a<b) {
+				System.out.println(-a+b);
+			}
+			
+			
+			}
+		}
+	}
