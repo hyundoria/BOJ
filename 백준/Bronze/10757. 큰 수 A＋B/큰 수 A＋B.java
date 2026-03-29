9a501d8308c35e9829523a1b643de56e336029e8
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:53:30 +0900

    [Bronze V] Title: 큰 수 A+B, Time: 368 ms, Memory: 21092 KB -BaekjoonHub

---
+++ 백준/Bronze/10757. 큰 수 A＋B/큰 수 A＋B.java
@@ -0,0 +1,16 @@
+
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        BigInteger bi = new BigInteger(sc.next());
+        BigInteger bii = new BigInteger(sc.next());
+
+        System.out.println(bi.add(bii));
+
+    }
+}
