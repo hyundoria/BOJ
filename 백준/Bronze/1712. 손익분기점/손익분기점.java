c5a493c0d1ff06f823255af434a71256bac2cb8e
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 12:16:43 +0900

    [Bronze II] Title: 손익분기점, Time: 208 ms, Memory: 17688 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			
+			int a = sc.nextInt();	//고정비용
+			int b = sc.nextInt();	//가변비용
+			int c = sc.nextInt();	//노트북 가격	
+						
+			int d = c-b;
+			
+			if (c <= b) {
+				System.out.println("-1");
+			}
+						
+			else{
+				System.out.println((a / d)+1);
+			}			
+						
+ 						
+			}
+		}
+	}
