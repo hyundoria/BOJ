84c9c2bb7d229168981ae823ed5e4111caebcbf2
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:53:21 +0900

    [Bronze V] Title: 세금, Time: 220 ms, Memory: 17664 KB -BaekjoonHub

---
+++ 백준/Bronze/20492. 세금/세금.java
@@ -0,0 +1,21 @@
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+
+        System.out.println(n*78/100);
+        System.out.println((n*4/5) + ((n/5) * 78/100));
+
+
+
+    }
+}
+
+
+
