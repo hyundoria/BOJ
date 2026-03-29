ed90795c8f2f7e99c4f3891d76280c7e6095b253
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:05:13 +0900

    [Bronze V] Title: 아스키 코드, Time: 216 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			
+			char n = sc.nextLine().charAt(0);
+			
+			System.out.println((int)n);
+			
+			
+			
+			
+			}
+		}
+	}
+
+
