d31de959fdd8dbb086fd5c6470b5a966cfa9184e
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:56:59 +0900

    [Silver II] Title: 알고리즘 수업 - 행렬 경로 문제 2, Time: 252 ms, Memory: 73876 KB -BaekjoonHub

---
+++ 백준/Silver/24419. 알고리즘 수업 － 행렬 경로 문제 2/알고리즘 수업 － 행렬 경로 문제 2.py
@@ -0,0 +1,50 @@
+import math
+
+def matrix_path1(m, n):# (1, 1)에서 (n, n)에 이르는 최고 점수를 구한다.
+    cnt = [0]
+    result =  matrix_path_rec(m, n, n, cnt)
+    print(int(cnt[0]/2) + 1)
+    return result
+
+def matrix_path_rec(m, i, j, cnt): # (1, 1)에서 (i, j)에 이르는 최고 점수를 구한다.
+    cnt[0] += 1
+    if i == 0 or j == 0:
+        return 0
+    else:
+        return m[i-1][j-1] + max(matrix_path_rec(m, i-1, j, cnt), matrix_path_rec(m, i, j-1, cnt))
+
+
+def matrix_path2(m, n):  # (1, 1)에서 (n, n)에 이르는 최고 점수를 구한다.
+
+    d = [[0] * (n+1) for _ in range(n+1)]
+    cnt = 0
+
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            d[i][j] = m[i-1][j-1] + max(d[i - 1][j], d[i][j - 1])  # 코드2
+            cnt += 1
+
+    print(cnt)
+    return d[n][n]
+
+
+n = int(input())
+
+arr = []
+
+for _ in range(n):
+    row = list(map(int, input().split()))
+    arr.append(row)
+
+MOD = 1000000007
+
+a = math.factorial(2*n) % MOD
+b = math.factorial(n) % MOD
+c = (b*b) % MOD
+d = pow(c, -1, MOD)
+
+re = (a * d) % MOD
+
+print(f'{re} {n**2}')
+
+
