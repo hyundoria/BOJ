0621f54ca2ba7d6e3a249436ab421f534b960134
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 15:00:40 +0900

    [Bronze V] Title: 문자와 문자열, Time: 212 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+
+public class Main {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        
+        String str[] = sc.next().split("");
+        int n = sc.nextInt();
+        
+        System.out.print(str[n-1]);
+    
+    
+    }
+
+}
