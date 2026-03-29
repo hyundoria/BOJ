5db22bc12dcac132be0829dc0554ff9cf12ff608
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:18:55 +0900

    [Bronze III] Title: 최댓값, Time: 208 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+		
+						
+						
+			int[]  arr = new int[9];
+			
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
+			}
+			
+			//
+			
+			int max = arr[0];			
+			int index = 0;
+			
+			for (int i = 0; i < arr.length; i++) {
+				if(arr[i] >= max) {
+					max = arr[i];
+					index = (i+1);
+				}
+			}
+			
+			
+			
+			
+			System.out.println(max);
+			System.out.println(index);
+			
+			
+		}		 
+	}
+}
