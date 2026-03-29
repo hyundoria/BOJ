f1f0acd5377ebbaba645802fb1e8e2a69481a494
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:38:44 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 188 ms, Memory: 17464 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			
+			System.out.println("       _.-;;-._ ");
+			System.out.println("'-..-'|   ||   |");
+			System.out.println("'-..-'|_.-;;-._|");
+			System.out.println("'-..-'|   ||   |");
+			System.out.println("'-..-'|_.-''-._|");
+		
+			
+			
+			
+				
+		}
+	}
+}
