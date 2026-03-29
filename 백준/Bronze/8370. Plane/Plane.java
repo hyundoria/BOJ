51c148529f5591947028c3b04e2d7ba4874c6826
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:44:54 +0900

    [Bronze V] Title: Plane, Time: 208 ms, Memory: 17748 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+				
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			int c = sc.nextInt();
+			int d = sc.nextInt();
+			
+			System.out.println(a*b + c*d);
+			
+			
+			}
+		}
+	}
+
+
