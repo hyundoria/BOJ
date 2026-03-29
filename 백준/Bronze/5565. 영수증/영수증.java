d6d936945e5a6e6c339e0f4ea5998e4081538ceb
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:40:22 +0900

    [Bronze IV] Title: 영수증, Time: 204 ms, Memory: 17696 KB -BaekjoonHub

---
+++ 백준/Bronze/5565. 영수증/영수증.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+					
+			int n = sc.nextInt();
+			
+			int sum = 0;
+			
+			for (int i = 0; i < 9; i++) {
+				sum += sc.nextInt();
+			}
+			
+			int value = n - sum;
+			System.out.println(value);
+			
+		
+		
+		}		
+	}
+}
+
