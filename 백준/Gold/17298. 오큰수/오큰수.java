dc301c3168936ef741b771f2a5c2074c6a12275e
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:50:51 +0900

    [Gold IV] Title: 오큰수, Time: 2216 ms, Memory: 321272 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,52 @@
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < n; i++) {
+
+            arr[i] = sc.nextInt();
+
+        }
+
+        for (int i = 0; i < n; i++) {
+
+
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+
+                arr[stack.pop()] = arr[i];
+
+            }
+
+            stack.push(i);
+
+        }
+
+        while(!stack.isEmpty()) {
+            arr[stack.pop()] = -1;
+        }
+
+
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < n; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.println(sb.toString().trim());
+
+        
+
+
+    }
+}
