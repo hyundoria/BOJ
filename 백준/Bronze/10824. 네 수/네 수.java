4c419c1709d68a941a40b443866550c857b073f9
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:56:24 +0900

    [Bronze III] Title: 네 수, Time: 196 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/10824. 네 수/네 수.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+			
+			String a = sc.next();
+			String b = sc.next();
+			String c = sc.next();
+			String d = sc.next();
+			
+			long e = Long.parseLong(a+b);
+			long f = Long.parseLong(c+d);
+			
+			
+			System.out.println(e+f);
+			
+			
+
+			
+			
+			
+			
+			
+			
+			}
+		}
+	}
+
+
+
