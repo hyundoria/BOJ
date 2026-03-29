027b92bd02f425b5ab31ec995d1931667fa7a8c6
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 13:44:44 +0900

    [Bronze V] Title: Can you add this?, Time: 224 ms, Memory: 17964 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {	 
+	public static void main(String[] args)  {
+		try (Scanner sc = new Scanner(System.in)) {
+			
+				
+			int n = sc.nextInt();
+			
+			for (int i = 0; i < n; i++) {
+				int a = sc.nextInt();
+				int b = sc.nextInt();
+				System.out.println(a+b);
+			}
+			
+			
+			
+			}
+		}
+	}
+
+
