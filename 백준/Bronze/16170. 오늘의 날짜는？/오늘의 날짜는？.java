c5d05175bd6e9ea3e38ddd961727f5ef540086ec
hyundoria <110231201+hyundoria@users.noreply.github.com>
2026-03-29 14:46:20 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 188 ms, Memory: 17556 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("2022");
+        System.out.println("07");
+        System.out.println("29");
+
+    }
+}
